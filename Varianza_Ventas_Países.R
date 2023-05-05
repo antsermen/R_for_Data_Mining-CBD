5608298215082aa698b05d31d6476b989820f9e6
Juan Carlos Moreno Pérez <73229466+juancarlos1907@users.noreply.github.com>
2023-05-05 19:36:35 +0200

    Varianza de las ventas por paises

---
+++ Varianza_Ventas_Países.R
@@ -0,0 +1,11 @@
+library(RSQLite)
+
+con <- dbConnect(RSQLite::SQLite(), "chinook.db")
+
+consulta <- "SELECT BillingCountry as País, (SUM(Total*Total)/COUNT(*) - (SUM(Total)/COUNT(*)) * (SUM(Total)/COUNT(*))) AS Varianza
+             FROM Invoices
+             GROUP BY País"
+
+resultados <- dbGetQuery(con, consulta)
+
+print(resultados)
