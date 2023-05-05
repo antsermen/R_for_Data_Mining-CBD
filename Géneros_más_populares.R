035897483ccfe06172320dc597856ea268d183cc
Antonio Roberto Serrano Mena <80272558+antsermen@users.noreply.github.com>
2023-05-05 14:21:07 +0200

    Pequeña mejora del script
    Ahora los resultados se guardan en dos variables "topCincoGenerosXVendidos", donde X puede ser "Mas" o "Menos", para una mejor visualización de dichos resultados.

--- Géneros_más_populares.R
+++ Géneros_más_populares.R
@@ -5,5 +5,7 @@ ventasPorGenero <- dbGetQuery(con, "SELECT g.Name as Género, COUNT(ii.InvoiceLi
                               FROM Genres g LEFT JOIN Tracks t ON t.GenreId = g.GenreId
                               LEFT JOIN Invoice_items ii ON ii.TrackId = t.TrackId GROUP BY g.Name
                               ORDER BY Ventas DESC")
-print("Los 5 géneros más vendidos son:"); print(head(ventasPorGenero,5))
-print("Los 5 géneros menos vendidos son:"); print(tail(ventasPorGenero,5))
+topCincoGenerosMasVendidos <- head(ventasPorGenero, 5)
+topCincoGenerosMenosVendidos <- tail(ventasPorGenero, 5)
+print("Los 5 géneros más vendidos junto al número total de ventas son:"); print(topCincoGenerosMasVendidos)
+print("Los 5 géneros menos vendidos junto al número total de ventas son:"); print(topCincoGenerosMenosVendidos)
