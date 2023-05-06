dc596400b2fba1c1aeb6fdda6e18555853052e88
Antonio Roberto Serrano Mena <80272558+antsermen@users.noreply.github.com>
2023-05-06 23:18:32 +0200

    Añadida desconexión con la base de datos

--- Analisis_Ventas_Tiempo.R
+++ Analisis_Ventas_Tiempo.R
@@ -7,3 +7,4 @@ data <- dbGetQuery(con, "SELECT Total, InvoiceDate FROM Invoices")
 ts_data <- ts(data$Total, frequency = 12, start = c(2010, 1), end = c(2020, 12))
 
 plot(ts_data, main = "Ventas en Chinook", xlab = "Año", ylab = "Ventas")
+dbDisconnect(con)
